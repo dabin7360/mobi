25e339280663c1ff0123068440c38fed38dcfe74
dabin7360 <524390074@qq.com>
2016-04-05 15:54:16 +0800

    iscroll & m_calender

--- app/platforms/ios/www/js/m-public.js
+++
@@ -1,8 +0,0 @@
-define(['../js/lib/zepto'],function(){
-	return function(){
-		$('header').append('<span class="goback"></span>')
-		.on('tap',function(){
-			window.history.back();
-		});
-	}
-})
--- app/www/js/public.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	$('header').append('<span class="goback"></span>')
-	.on('click',function(){
-		window.history.back();
-	});
-})
